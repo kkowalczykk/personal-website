0e85f09a0f95ba822a4f5e94c0a32a0ff2f24df6
KarolKowalczyk <kovalczyk97@gmail.com>
2023-01-29 00:08:35 +0100

    build: hide/show navbar on scroll

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Burger from '../burger/Burger';
 import NavbarMenu from '../navbarMenu/NavbarMenu';
 
@@ -7,9 +8,52 @@ export interface INavbar {
 }
 
 const Navbar: React.FC<INavbar> = ({ children }) => {
+  useEffect(() => {
+    let scrollPos = window.pageYOffset;
+    const navbarScrollEffect = (event: Event) => {
+      const navbar = document.querySelector('.navbar');
+      const navbarHeight = navbar?.clientHeight;
+
+      let currentScrollPos = window.pageYOffset;
+
+      // Hide navbar on scroll down
+      if (scrollPos > currentScrollPos) {
+        navbar?.classList.remove('-translate-y-full');
+      } else {
+        navbar?.classList.add('-translate-y-full');
+      }
+      scrollPos = currentScrollPos;
+
+      // If page is scrolled down, change navbar appearance
+      if (navbarHeight) {
+        if (window.scrollY > navbarHeight) {
+          navbar?.classList.add(
+            'bg-dark-primary/80',
+            'backdrop-blur-md',
+            'shadow-lg'
+          );
+          navbar?.classList.remove('py-2');
+        } else {
+          navbar?.classList.remove(
+            'bg-dark-primary/80',
+            'backdrop-blur-md',
+            'shadow-lg'
+          );
+          navbar?.classList.add('py-2');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', navbarScrollEffect);
+
+    return () => {
+      window.removeEventListener('scroll', navbarScrollEffect);
+    };
+  }, []);
+
   return (
-    <nav className="fixed left-0 top-0 z-30 flex w-full flex-1 flex-row items-center justify-between px-5 text-center text-white lg:px-20">
-      <div className="container mx-auto flex justify-between py-3">
+    <nav className="navbar fixed left-0 top-0 z-30 flex w-full flex-1 flex-row items-center justify-between px-5 text-center text-white transition-all duration-300 lg:px-20">
+      <div className="container mx-auto flex justify-between py-4">
         <div className="flex flex-row items-center justify-center">
           <Link href="/">
             <div className="flex flex-row items-center justify-center">
